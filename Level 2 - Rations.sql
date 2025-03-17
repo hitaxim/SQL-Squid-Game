c2f65849a131ca200c7da9a8b147a0b950894af3
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 15:14:58 -0700

    Update Level 2 - Rations.sql

--- Level 2 - Rations.sql
+++ Level 2 - Rations.sql
@@ -1,1 +1,7 @@
-
+SELECT 
+	FLOOR(COUNT(*) * 0.90) AS "Needed Rations", 
+	FLOOR(COUNT(*) * 0.90) <= (SELECT amount FROM rations) AS "Sufficient"
+FROM player
+WHERE status = 'alive' AND IsInsider = False;
+                
+             
